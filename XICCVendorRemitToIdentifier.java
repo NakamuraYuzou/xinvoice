3bf1cdf0f66e0b23d09160b7c2ff9d1c777c4140
Nakamura <y-nakamura@sadenko.co.jp>
2025-05-05 14:29:43 +0900

    XICCVendorRemitToIdentifier

---
+++ XICCVendorRemitToIdentifier.java
@@ -0,0 +1,21 @@
+package xinvoice;
+
+public class XICCVendorRemitToIdentifier
+{
+	public String VendorCode;
+
+	public XICCVendorRemitToIdentifier()
+	{
+		VendorCode = "";
+	}
+
+	public String getVendorCode()
+	{
+		return( VendorCode );
+	}
+
+	public void setVendorCode( String arg )
+	{
+		VendorCode = arg;
+	}
+}
