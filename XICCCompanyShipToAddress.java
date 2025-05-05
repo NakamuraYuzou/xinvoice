ccedee6b1a5a66c796712408780f8240c6a39269
Nakamura <y-nakamura@sadenko.co.jp>
2025-05-05 14:07:57 +0900

    XICCCompanyShipToAddress

---
+++ XICCCompanyShipToAddress.java
@@ -0,0 +1,22 @@
+package xinvoice;
+
+public class XICCCompanyShipToAddress
+{
+	public String Name;
+	public String Address1;
+	public String City;
+	public String State;
+	public String PostalCode;
+	public String CountryCode;
+
+	public XICCCompanyShipToAddress()
+	{
+		Name = "Š”®‰ïĞ²“dH";
+		Address1 = "“V_ˆê’š–Ú4-3";
+		City = "²‰ês";
+		State = "²‰êŒ§";
+		PostalCode = "840-0815";
+		CountryCode = "JP";
+	}
+
+}
