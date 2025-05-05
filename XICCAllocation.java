464fae191626246bdfdea90bd59dd3df7d81ca4f
Nakamura <y-nakamura@sadenko.co.jp>
2025-05-05 14:05:53 +0900

    XICCAllocation

---
+++ XICCAllocation.java
@@ -0,0 +1,22 @@
+package xinvoice;
+
+public class XICCAllocation
+{
+	public String Percentage;
+
+	public XICCAllocation()
+	{
+		Percentage = "";
+	}
+
+	public String getPercentage()
+	{
+		return( Percentage );
+	}
+
+	public void setPercentage( String arg )
+	{
+		Percentage = arg;
+	}
+
+}
